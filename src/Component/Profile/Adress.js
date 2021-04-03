7c8f8bedf0150722daccbe6ef07e1d0c17ef9ae8
nour2297 <nourmezghaniGit22@gmail.com>
2021-04-03 11:53:46 +0100

    myprofile

---
+++ src/Component/Profile/Adress.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Adress = () => {
+    return ( <
+        div >
+        <
+        p > Route Tunis Km5 .5, Sfax < /p>   <
+        /div>
+    )
+}
+
+export default Adress
---
+++ src/Component/Profile/ProfilPhoto.js
@@ -0,0 +1,15 @@
+import React from 'react'
+const ProfilPhoto = () => {
+    return ( < div >
+        <
+        img src = "/nourphoto.jpg"
+        alt = ""
+        style = {
+            { maxWidth: "100px", maxHieght: "100px" }
+        }
+        /> < /
+        div >
+    )
+}
+
+export default ProfilPhoto
